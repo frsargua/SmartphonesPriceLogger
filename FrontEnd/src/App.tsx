df4246b08e9d8d6850490dfc1bfb07d98e647d0f
frsargua <frsargua@gmail.com>
2022-12-19 18:29:56 +0000

    Routes connecting  for connecting the pages (created)

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,18 +1,26 @@
+import { Container } from "@mui/system";
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { CreateBrand } from "./Pages/CreateBrand";
+import { Main } from "./Pages/Main";
+import { Prices } from "./Pages/Prices";
+import { UpdatePhone } from "./Pages/UpdatePhone/Index";
+import { UpdatePrice } from "./Pages/UpdatePrice/Index";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" />
-        <Route path="/create-brand" element={<CreateBrand />} />
-        <Route path="/" />
-        <Route path="/" />
-      </Routes>
+      <Container maxWidth="lg">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/create-brand" element={<CreateBrand />} />
+          <Route path="/update-phone/:id" element={<UpdatePhone />} />
+          <Route path="/update-price/:id" element={<UpdatePrice />} />
+          <Route path="/prices/:id" element={<Prices />} />
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 }
