f380e2c7bad1e54011587f3d58cb797e403f627d
frsargua <frsargua@gmail.com>
2022-12-20 17:51:11 +0000

    Created linechart component

---
+++ FrontEnd/src/components/LineChart/Index.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import type { ChartData, ChartOptions } from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+interface LineProps {
+  options: ChartOptions<"line">;
+  data: ChartData<"line">;
+}
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: false,
+    },
+  },
+};
+
+interface Props {
+  labels?: string[];
+  devaluationData?: number[];
+}
+
+export function LineChart(props: Props) {
+  const { labels, devaluationData } = props;
+  let data = {
+    labels,
+    datasets: [
+      {
+        label: "name",
+        data: devaluationData,
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+  return <Line options={options} data={data} />;
+}
