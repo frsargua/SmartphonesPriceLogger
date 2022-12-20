990585d6b0fb82f9ebe5fe67dabb478df3a40f56
frsargua <frsargua@gmail.com>
2022-12-20 16:28:26 +0000

    added update phone functionality

--- FrontEnd/src/Pages/UpdatePhone/Index.tsx
+++ FrontEnd/src/Pages/UpdatePhone/Index.tsx
@@ -1,7 +1,69 @@
+import * as React from "react";
 import { Button, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { useContext } from "react";
+import { BrandsContext } from "../../context/BrandsContext";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import { CollectionOfPhonesContext } from "../../context/PhoneListContext";
+import { useParams } from "react-router-dom";
+import { MyParams } from "../../types";
+import { useNavigate } from "react-router-dom";
 
 export function UpdatePhone() {
+  const navigate = useNavigate();
+  const { id } = useParams<keyof MyParams>() as MyParams;
+  let { brands } = useContext(BrandsContext);
+  let { phones, fetchPhones } = React.useContext(CollectionOfPhonesContext);
+  const [brand, setBrand] = React.useState<string>("");
+  const [model, setModel] = React.useState<string>();
+  const [price, setPrice] = React.useState<Number | null>();
+
+  const handleChangeSelect = (event: SelectChangeEvent) => {
+    setBrand(event.target.value as string);
+  };
+  function handleModelChange(event: React.ChangeEvent<any>) {
+    setModel(event.target.value as string);
+  }
+  function handlePriceChange(event: React.ChangeEvent<any>) {
+    setPrice(event.target.value as Number);
+  }
+
+  async function handleSubmit(event: React.ChangeEvent<any>) {
+    event.preventDefault();
+
+    let body = {
+      brand_name: brand,
+      model: model,
+      release_price: price,
+    };
+    try {
+      let response = await fetch(`http://127.0.0.1:8000/api/phones/${id}`, {
+        method: "PUT",
+        body: JSON.stringify({ ...body }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (response) {
+        fetchPhones();
+        navigate(`/`, { replace: true });
+      }
+    } catch (error) {
+      console.error("Error in POST request:", error);
+      return;
+    }
+  }
+
+  React.useEffect(() => {
+    console.log(phones);
+    let singlePhone = phones.filter((el) => el.id == parseInt(id))[0];
+    setBrand("Apple");
+    setModel(singlePhone.model);
+    setPrice(singlePhone.release_price);
+  }, []);
+
   return (
     <>
       <Box
@@ -26,16 +88,49 @@ export function UpdatePhone() {
           Change Details
         </Typography>
         <Box
+          component="form"
+          onSubmit={handleSubmit}
           sx={{
             display: "flex",
             flexDirection: "column",
             width: "100%",
           }}
         >
-          <TextField label="Brand" variant="filled" margin="normal" />
-          <TextField label="Model" variant="filled" margin="normal" />
-          <TextField label="Price" variant="filled" margin="normal" />
-          <Button variant="contained">Change</Button>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Brand</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={brand}
+              variant="filled"
+              size="small"
+              label="Brand"
+              onChange={handleChangeSelect}
+            >
+              {brands.map((el, i) => (
+                <MenuItem key={i} value={el.brand}>
+                  {el.brand}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>{" "}
+          <TextField
+            label="Model"
+            value={model}
+            onChange={handleModelChange}
+            variant="filled"
+            margin="normal"
+          />
+          <TextField
+            label="Price"
+            value={price}
+            onChange={handlePriceChange}
+            variant="filled"
+            margin="normal"
+          />
+          <Button type="submit" variant="contained">
+            Change
+          </Button>
         </Box>
       </Box>
     </>
