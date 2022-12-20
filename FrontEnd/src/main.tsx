02b758b70266b7165225829ce792fd7514a8e079
frsargua <frsargua@gmail.com>
2022-12-20 19:37:11 +0000

    Added linear graph for data

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrandsProvider } from "./context/BrandsContext";
+import { CollectionOfPhonesProvider } from "./context/PhoneListContext";
+import { PricesProvider } from "./context/PricesContext";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrandsProvider>
+      <PricesProvider>
+        <CollectionOfPhonesProvider>
+          <App />
+        </CollectionOfPhonesProvider>
+      </PricesProvider>
+    </BrandsProvider>
+  </React.StrictMode>
+);
