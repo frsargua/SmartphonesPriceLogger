53a6e920217e6bf60972cae16f4c38ff3cf3e5b4
frsargua <frsargua@gmail.com>
2022-12-21 16:29:46 +0000

    created temp id ( not used at the end)

--- FrontEnd/src/context/PricesContext.tsx
+++ FrontEnd/src/context/PricesContext.tsx
@@ -6,20 +6,32 @@ import { getAllPricesById } from "../utils/URIs";
 
 export const PricesContext = createContext<PricesContextProps>({
   prices: [],
+  tempId: "",
   fetchPrices: () => {},
+  changeTempId: () => {},
 });
 
 export const PricesProvider = ({ children }: ChildrenProps) => {
   const [prices, setPrices] = useState<PricesProps[]>([]);
+  const [tempId, setTempId] = useState<string>("");
 
   async function fetchPrices(id: string) {
     let data = await fetchData(getAllPricesById(id));
     setPrices(data);
   }
 
+  async function changeTempId(id: string) {
+    let data = await fetchData(getAllPricesById(id));
+    let newData = prices.filter((el) => el.id != id);
+    console.log(data);
+    setPrices(newData);
+  }
+
   let value = {
     prices: prices,
     fetchPrices: fetchPrices,
+    tempId: tempId,
+    changeTempId: changeTempId,
   };
   return (
     <PricesContext.Provider value={value}>{children}</PricesContext.Provider>
